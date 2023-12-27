56ddb0ce51a7a98d11617227e4d844ca0c546482
MaHyaR Emadi <emadi.mahyar@gmail.com>
2023-12-27 20:39:16 +0330

    feat : cooperation completed

--- assets/js/script.js
+++ assets/js/script.js
@@ -19,3 +19,21 @@ const $header = document.querySelector("[data-header]");
 window.addEventListener("scroll", e => {
     $header.classList[window.scrollY > 30 ? "add" : "remove"]("active");
 });
+
+
+
+// let valueDisplays = document.querySelectorAll(".num");
+// let interval = 3000;
+// valueDisplays.forEach((valueDisplay) => {
+//     let startvalue = 0;
+//     let endvalue = parseInt(valueDisplay.getAttribute("data-val"));
+//     let duration = math.floor(interval / endvalue);
+//     let counter = setInterval (function () {
+//         startvalue += 1 ;
+//         valueDisplay.textContent = startvalue ;
+//         if ( startvalue == endvalue) {
+//             clearInterval(counter) ;
+//         }
+
+//     }, duration);
+// });
