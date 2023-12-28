e20c071947fb422c2fc264fa907b7cd11f40e11b
MaHyaR Emadi <emadi.mahyar@gmail.com>
2023-12-28 16:09:21 +0330

    fix : number counting fixed

--- assets/js/script.js
+++ assets/js/script.js
@@ -30,18 +30,22 @@ $toggleBtns.forEach($toggleBtns =>{
    });
 });
 
-// let valueDisplays = document.querySelectorAll(".num");
-// let interval = 3000;
-// valueDisplays.forEach((valueDisplay) => {
-//     let startvalue = 0;
-//     let endvalue = parseInt(valueDisplay.getAttribute("data-val"));
-//     let duration = math.floor(interval / endvalue);
-//     let counter = setInterval (function () {
-//         startvalue += 1 ;
-//         valueDisplay.textContent = startvalue ;
-//         if ( startvalue == endvalue) {
-//             clearInterval(counter) ;
-//         }
+// add Number counting animation
 
-//     }, duration);
-// });
+let valueDisplays = document.querySelectorAll(".num");
+let interval = 3000;
+
+valueDisplays.forEach((valueDisplay) => {
+    let startvalue = 0;
+    let endvalue = parseInt(valueDisplay.getAttribute("data-val"));
+    console.log(endvalue);
+    let duration = Math.floor(interval / endvalue);
+    let counter = setInterval (function () {
+        startvalue += 1 ;
+        valueDisplay.textContent = startvalue ;
+        if ( startvalue == endvalue) {
+            clearInterval(counter) ;
+        }
+
+    }, duration);
+});
